f929412b5ea917ae820072d7ac7e2f7a0d690c92
Isaac Garro <isaacgarro21@gmail.com>
2021-10-15 21:36:34 -0600

    Adding Database and Ingredient Entity

---
+++ Models/Ingredient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanaderiaIkigai.Models
+{
+    class Ingredient
+    {
+        /// <summary>
+        /// Unique Ingredient Code
+        /// </summary>
+        public int Code { get; set; }
+        /// <summary>
+        /// Ingredient Name
+        /// </summary>
+        public int Name { get; set; }
+        public string UnitMeasure { get; set; }
+        public int IndividualUnits { get; set; }
+        public int TotalUnits { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal PricePerGram { get; set; }
+        public decimal AmountInUnit { get; set; }
+        public string IngredientSource { get; set; }
+        public int Quality { get; set; }
+    }
+}
