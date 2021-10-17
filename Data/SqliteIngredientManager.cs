38c16db713aedb9150f9570a6055b0b80652b15e
Isaac Garro <dasphillipbrau@outlook.com>
2021-10-17 11:04:07 -0600

    Added new attributes for detailed ingredients. Modified logic to retrieve a detailed ingredient from database. Added method to retrieve every detailed ingredient.

--- Data/SqliteIngredientManager.cs
+++ Data/SqliteIngredientManager.cs
@@ -45,7 +45,11 @@ namespace PanaderiaIkigai.Data
                 throw ex;
             }
         }
-
+        /// <summary>
+        /// Accesses the database and retrieves every detailed ingredient based on a specific base ingredient code.
+        /// </summary>
+        /// <param name="pBaseIngredient">The base ingredient to filter on.</param>
+        /// <returns>List containing every detailed ingredient related to the base ingredient used as filter.</returns>
         public IEnumerable<DetailedIngredient> GetDetailedIngredients(BaseIngredient pBaseIngredient)
         {
             try
@@ -53,15 +57,15 @@ namespace PanaderiaIkigai.Data
                 using(var conn = new SqliteConnection(GetConnectionString()))
                 {
                     List<DetailedIngredient> detailedIngredientsList = new List<DetailedIngredient>();
-                    var getDetailedIngredientsCommand = new SqliteCommand("SELECT INGREDIENT_CODE, INGREDIENT_SOURCE, " +
+                    var getDetailedIngredientsCommand = new SqliteCommand("SELECT INGREDIENT_CODE, DETAILED_INGREDIENT_ID, BRAND, INGREDIENT_SOURCE, " +
                         "UNIT_PRICE, AMOUNT_IN_UNIT, MINIMUM_PRICE_PER_UNIT, QUALITY, UNITS_AVAILABLE FROM INGREDIENT_DETAILED WHERE INGREDIENT_CODE = $code", conn);
                     getDetailedIngredientsCommand.Parameters.AddWithValue("$code", pBaseIngredient.Code);
                     var reader = getDetailedIngredientsCommand.ExecuteReader();
                     while (reader.Read())
                     {
-                        var detailedIngredient = new DetailedIngredient(pBaseIngredient, reader.GetString(1), reader.GetDecimal(2),
-                            reader.GetDecimal(3), reader.GetDecimal(4), reader.GetInt32(5));
-                        var unitsAvailable = reader.GetInt32(6);
+                        var detailedIngredient = new DetailedIngredient(pBaseIngredient, reader.GetString(1), reader.GetString(2), reader.GetString(3),
+                            reader.GetDecimal(4), reader.GetDecimal(5), reader.GetDecimal(6), reader.GetInt32(7));
+                        var unitsAvailable = reader.GetInt32(8);
                         detailedIngredient.UnitsAvailable = unitsAvailable;
                         detailedIngredientsList.Add(detailedIngredient);
                     }
@@ -73,6 +77,48 @@ namespace PanaderiaIkigai.Data
                 Console.WriteLine(sqlEx.ErrorCode);
                 throw sqlEx;
             } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Accesses the database and retrieves every detailed ingredient regardless of base ingredient.
+        /// </summary>
+        /// <returns>All the records in the detailed ingredients table</returns>
+        public IEnumerable<DetailedIngredient> GetDetailedIngredients()
+        {
+            try
+            {
+                using (var conn = new SqliteConnection(GetConnectionString()))
+                {
+                    List<DetailedIngredient> detailedIngredientsList = new List<DetailedIngredient>();
+                    var getDetailedIngredientsCommand = new SqliteCommand("SELECT A.INGREDIENT_CODE, A.DETAILED_INGREDIENT_ID, A.BRAND, A.INGREDIENT_SOURCE, A.UNIT_PRICE, A.AMOUNT_IN_UNIT, " +
+                        "A.MINIMUM_PRICE_PER_UNIT, A.QUALITY, A.UNITS_AVAILABLE, B.NAME, B.UNIT_OF_MEASURE, B.TOTAL_UNITS_AVAILABLE " +
+                        "FROM INGREDIENT_DETAILED A " +
+                        "INNER JOIN INGREDIENT B" +
+                        "ON A.INGREDIENT_CODE = B.CODE" +
+                        "ORDER BY A.INGREDIENT_CODE ASC", conn);
+                    var reader = getDetailedIngredientsCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var baseIngredient = new BaseIngredient(reader.GetInt32(0), reader.GetString(9), reader.GetString(10), reader.GetInt32(11));
+                        var detailedIngredient = new DetailedIngredient(baseIngredient, reader.GetString(1),
+                            reader.GetString(2), reader.GetString(3), reader.GetDecimal(4), reader.GetDecimal(5), reader.GetDecimal(6), reader.GetInt32(7));
+                        var unitsAvailable = reader.GetInt32(8);
+                        detailedIngredient.UnitsAvailable = unitsAvailable;
+                        detailedIngredientsList.Add(detailedIngredient);
+                    }
+                    return detailedIngredientsList;
+                }
+            }
+            catch (SqliteException sqlEx)
+            {
+                Console.WriteLine(sqlEx.Message);
+                Console.WriteLine(sqlEx.ErrorCode);
+                throw sqlEx;
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 throw ex;
--- Models/DetailedIngredient.cs
+++ Models/DetailedIngredient.cs
@@ -8,6 +8,8 @@ namespace PanaderiaIkigai.Models
 {
     public class DetailedIngredient : BaseIngredient
     {
+        public string DetailedIngredientCode { get; set; }
+        public string Brand { get; set; }
         public string IngredientSource { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal AmountInUnit { get; set; }
@@ -16,9 +18,11 @@ namespace PanaderiaIkigai.Models
 
         public DetailedIngredient() { }
 
-        public DetailedIngredient(int pCode, string pName, string pUnit, int pUnitsAvailable, string pIngredientSource, 
+        public DetailedIngredient(int pCode, string pName, string pUnit, int pUnitsAvailable, string pBrand, string pDetailedIngredientCode, string pIngredientSource, 
             decimal pUnitPrice, decimal pAmountInUnit, decimal pMinimumUnitPrice, int pQuality) : base(pCode, pName, pUnit, pUnitsAvailable)
         {
+            DetailedIngredientCode = pDetailedIngredientCode;
+            Brand = pBrand;
             IngredientSource = pIngredientSource;
             UnitPrice = pUnitPrice;
             AmountInUnit = pAmountInUnit;
@@ -26,10 +30,12 @@ namespace PanaderiaIkigai.Models
             Quality = pQuality;
         }
 
-        public DetailedIngredient(BaseIngredient pBaseIngredient, string pIngredientSource,
+        public DetailedIngredient(BaseIngredient pBaseIngredient, string pDetailedIngredientCode, string pBrand, string pIngredientSource,
             decimal pUnitPrice, decimal pAmountInUnit, decimal pMinimumUnitPrice, int pQuality) : base(pBaseIngredient.Code, pBaseIngredient.Name, 
                 pBaseIngredient.MeasuringUnit, pBaseIngredient.UnitsAvailable)
         {
+            DetailedIngredientCode = pDetailedIngredientCode;
+            Brand = pBrand;
             IngredientSource = pIngredientSource;
             UnitPrice = pUnitPrice;
             AmountInUnit = pAmountInUnit;
