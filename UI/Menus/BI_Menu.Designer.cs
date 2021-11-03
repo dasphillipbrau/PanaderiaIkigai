dd07fafd2afcaf8dd3a446f644675308c66e4ace
Isaac Garro <isaacgarro21@gmail.com>
2021-11-03 11:49:24 -0600

    adding livecharts

---
+++ UI/Menus/BI_Menu.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace PanaderiaIkigai.UI.Menus
+{
+    partial class BI_Menu
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pieChart1 = new LiveCharts.WinForms.PieChart();
+            this.SuspendLayout();
+            // 
+            // pieChart1
+            // 
+            this.pieChart1.Location = new System.Drawing.Point(33, 21);
+            this.pieChart1.Name = "pieChart1";
+            this.pieChart1.Size = new System.Drawing.Size(413, 282);
+            this.pieChart1.TabIndex = 0;
+            this.pieChart1.Text = "pieChart1";
+            // 
+            // BI_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(76)))), ((int)(((byte)(103)))));
+            this.Controls.Add(this.pieChart1);
+            this.Name = "BI_Menu";
+            this.Size = new System.Drawing.Size(1235, 580);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.PieChart pieChart1;
+    }
+}
